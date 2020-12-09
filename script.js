0385248bb0a463826b6ea13a4404c9c573b2f0d7
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-09 03:55:41 +0000

    Textbox

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ document.getElementById('mybtn').addEventListener('click', displayDate)
 function displayDate () {
   document.getElementById('demo').innerHTML = Date()
 }
+
+function textBox() {
+  document.getElementById("myText").value = "Goodbye!";
+}
